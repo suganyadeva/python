8bacbf7f03f2805a7688d2e5cd97f4b4c1afce1c
suganyadeva <suganyadeva13@gmail.com>
2014-01-28 15:35:22 +0530

    Create squ.py

---
+++ squ.py
@@ -0,0 +1,20 @@
+#PROGRAM TO DISPLAY THE SQUARE
+for i in range(5):
+ for j in range(5):
+  if(i==4 or j==4 or i==0 or j==0):
+   print(" *",end='')
+  else: 
+   print("  ",end='')
+ print()
+print("\n")
+for i in range(7):
+ for j in range(7):
+  if(i==6 or j==6 or i==0 or j==0 or i==1 or j==1 or i==5 or j==5):
+   print(" *",end='')
+  else: 
+   print("  ",end='')
+ print()
+  
+ 
+  
+ 
