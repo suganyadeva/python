609269ae86fdfb9df7dc7038adf4e92eabe3698f
suganyadeva <suganyadeva13@gmail.com>
2014-01-28 15:26:43 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,10 @@
+a=[4,7,23,87,23]#for numbers
+i=eval(input("enter i:")) 	
+print(a[i])
+b=["aaa","bbb","xxx","ttt","sss","lll"]#for string
+j=eval(input("enter j:"))
+print(b[j])
+a.insert(5,50)#to insert value in a list
+print(a)
+b.insert(6,"rrr")
+print(b)
