a2914b50816dccc214a2cdc0fd4eea46637e5b70
suganyadeva <suganyadeva13@gmail.com>
2014-01-28 15:25:39 +0530

    Create large.py

---
+++ large.py
@@ -0,0 +1,12 @@
+#PROGRAM TO FIND THE LARGER NUMBER
+a=eval(input("enter a:"))
+b=eval(input("enter b:"))
+c=eval(input("enter c:"))
+if(a<b):
+ if(b>c):
+  print("b is greater")
+ elif(b<c):
+  print("c is greater")
+else:
+ print("a is greater")
+ 
