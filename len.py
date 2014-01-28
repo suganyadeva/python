eb527e3f7383e44261efa46cc0da5083f580c7c0
suganyadeva <suganyadeva13@gmail.com>
2014-01-28 15:26:10 +0530

    Create len.py

---
+++ len.py
@@ -0,0 +1,8 @@
+#PROGRAM TO DISPLAY THE LENGTH
+a=("a","b","c","d")
+print("length of a is:",len(a))
+print("\n")
+b=("apple\n","orange\n","grapes\n","pineapple\n","lemon\n")
+for i in b:
+ print(i)
+print("length of b is:", len(b))
