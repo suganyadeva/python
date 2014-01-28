44f33c44b56fc0cc0b9f6cca7608bcea48db207b
suganyadeva <suganyadeva13@gmail.com>
2014-01-28 15:23:55 +0530

    Create if.py

---
+++ if.py
@@ -0,0 +1,10 @@
+#PROGRAM USING if LOOP
+a=eval(input("enter a:"))
+b=eval(input("enter b:"))
+c=eval(input("enter c:"))
+if(a>b):
+ print("a is greater")
+elif(a<b):
+ print("b is greater")
+else:
+ print("c is greater")
