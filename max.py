e72dda4d1c15d821126fc92f15d28ffa31b8c4ac
suganyadeva <suganyadeva13@gmail.com>
2014-01-28 15:31:46 +0530

    Create max.py

---
+++ max.py
@@ -0,0 +1,6 @@
+#PROGRAM TO DISPLAY THE MAXIMUM VALUE USING math FUNCTION
+import math
+a=eval(input("enter a:"))
+b=eval(input("enter b:"))  
+c=eval(input("enter c:"))
+print("max value is:",max(a,b,c))
